6c9c312797a4e11cbaf5cc40fdc593fab20c6e4a
akshay898 <makshayyadav898@gmail.com>
2024-10-28 01:54:41 -0300

    commit

--- app.js
+++ app.js
@@ -1,47 +1,23 @@
-window.onscroll = function () {headersticky()};
-var header = document.getElementById("Anizoheader")
-var sticky= header.offsetTop;
-
-function headersticky(){
-  if (window.scrollY > sticky){
-    header.classList.add("sticky-header");
-  }
-  else{
-    header.classList.remove("sticky");
-  }
-}
-
-
-
-function isElementInViewport(elem) {
-  var $elem = $(elem);
-
-  // Get the scroll position of the page.
-  var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-  var viewportTop = $(scrollElem).scrollTop();
-  var viewportBottom = viewportTop + $(window).height();
-
-  // Get the position of the element on the page.
-  var elemTop = Math.round( $elem.offset().top );
-  var elemBottom = elemTop + $elem.height();
-
-  return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
-}
-
-// Check if it's time to start the animation.
-function checkAnimation() {
-  var $elem = $('.normal .static');
-
-  // If the animation has already been started
-  if ($elem.hasClass('animated')) return;
-
-  if (isElementInViewport($elem)) {
-      // Start the animation
-      $elem.addClass('animated');
-  }
-}
-
-// Capture scroll events
-$(window).scroll(function(){
-  checkAnimation();
-});
+$(document).ready(function() {
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout: 5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        },
+        1400:{
+          items:3
+      }
+    }
+  });
+});  
